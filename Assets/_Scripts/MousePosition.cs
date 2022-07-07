cf609d52d589ebe9e8d85590251baafb85def769
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-07-07 23:21:02 +0700

    Track mouse point in world scene

---
+++ Assets/_Scripts/MousePosition.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MousePosition : MonoBehaviour
+{
+    public static MousePosition instance;
+    [SerializeField] LayerMask mousePlaneLayer;
+
+    void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = MousePosition.GetPosition();
+    }
+
+    public static Vector3 GetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hitInfo, float.MaxValue, instance.mousePlaneLayer);
+        return hitInfo.point;
+    }
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector3 targetPosition;
+    float stoppingDistance = .1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 desiredDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += desiredDirection * moveSpeed * Time.deltaTime;
+        }
+
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            Move(new Vector3(4, 0, 4));
+        }
+    }
+
+    void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
     }
 }
