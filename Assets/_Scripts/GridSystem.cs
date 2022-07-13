70e417b581e409113dbd1ab24d37baa7b45c8e1f
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-07-13 21:43:45 +0700

    class GridSystem

---
+++ Assets/_Scripts/GridSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridSystem
+{
+    float width;
+    float height;
+    int cellSize;
+
+    public GridSystem(float width, float height, int cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                Debug.DrawLine(GetWorldPosition(x, z), GetWorldPosition(x, z) + Vector3.right * .5f, Color.cyan, float.MaxValue);
+            }
+        }
+    }
+
+    public Vector3 GetWorldPosition(int x, int z)
+    {
+        return new Vector3(x, 0, z) * cellSize;
+    }
+}
--- Assets/_Scripts/Test.cs
+++ Assets/_Scripts/Test.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    Camera cam;
-    Vector3 pos = new Vector3(200, 200, 0);
-
-
     void Start()
     {
-        cam = GetComponent<Camera>();
+        new GridSystem(10, 10, 2);
     }
 
     void Update()
     {
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+       
     }
 }
