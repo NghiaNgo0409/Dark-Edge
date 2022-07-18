a7f8ecf338073d58e631e30f1156b473c47b6db2
NghiaNgo0409 <87489313+NghiaNgo0409@users.noreply.github.com>
2022-07-18 20:40:09 +0700

    Change script name

--- Assets/_Scripts/Unit.cs
+++ Assets/_Scripts/Unit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class Unit : MonoBehaviour
 {
     Vector3 targetPosition;
     float stoppingDistance = .1f;
